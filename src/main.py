0ebe1ad37680355dc8cb56d21deced799f051602
Sumanth <93238666+Sumanth1680@users.noreply.github.com>
2022-10-08 17:14:26 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -11,9 +11,45 @@ from tkinter import filedialog
 from tkinter import *
 from PIL import ImageTk, Image
 
-def upload():
+def uploadToCartoonify():
     ImagePath=easygui.fileopenbox()
     cartoonify(ImagePath)
+
+# def uploadToColorize():
+#     ImagePath=easygui.fileopenbox()
+#     Colorize(ImagePath)
+
+def Colorize(ImagePath):
+    net = cv2.dnn.readNetFromCaffe('G:/Projects/PIXLAB/Colorize/colorization_deploy_v2.prototxt', 'G:/Projects/PIXLAB/Colorize/colorization_release_v2.caffemodel')
+    pts = np.load('G:/Projects/PIXLAB/Colorize/pts_in_hull.npy')
+    class8 = net.getLayerId("class8_ab")
+    conv8 = net.getLayerId("conv8_313_rh")
+    pts = pts.transpose().reshape(2, 313, 1, 1)
+    net.getLayer(class8).blobs = [pts.astype("float32")]
+    net.getLayer(conv8).blobs = [np.full([1, 313], 2.606, dtype='float32')]
+    image = cv2.imread(ImagePath)
+    scaled = image.astype("float32") / 255.0
+    lab = cv2.cvtColor(scaled, cv2.COLOR_BGR2LAB)
+    resized = cv2.resize(lab, (224, 224))
+    L = cv2.split(resized)[0]
+    L -= 50
+    net.setInput(cv2.dnn.blobFromImage(L))
+    ab = net.forward()[0, :, :, :].transpose((1, 2, 0))
+
+    ab = cv2.resize(ab, (image.shape[1], image.shape[0]))
+
+    L = cv2.split(lab)[0]
+    colorized = np.concatenate((L[:, :, np.newaxis], ab), axis=2)
+
+    colorized = cv2.cvtColor(colorized, cv2.COLOR_LAB2BGR)
+    colorized = np.clip(colorized, 0, 1)
+
+    colorized = (255 * colorized).astype("uint8")
+
+    cv2.imshow("Original", image)
+    cv2.imshow("Colorized", colorized)
+    cv2.waitKey(0)
+
 
 def cartoonify(ImagePath):
     originalimage = cv2.imread(ImagePath)
@@ -66,6 +102,17 @@ print("2. Colorize The Image")
 print("3. Cartoonify the Image")
 n = int(input())
 
+if n==2:
+    # top = tk.Tk()
+    # top.geometry('400x400')
+    # top.title('Colorize your Image !')
+    # top.configure(background='white')
+    # label = Label(top, background='#CDCDCD', font=('calibri', 20, 'bold'))
+    # upload = Button(top, text="Colorize an Image", command=uploadToColorize, padx=10, pady=5)
+    # upload.configure(background='#364156', foreground='white', font=('calibri', 10, 'bold'))
+    # upload.pack(side=TOP, pady=50)
+    # top.mainloop()
+    Colorize("G:/Projects/PIXLAB/Colorize/image1.jpg")
 
 if n==3:
     top = tk.Tk()
@@ -73,7 +120,7 @@ if n==3:
     top.title('Cartoonify your Image !')
     top.configure(background='white')
     label = Label(top, background='#CDCDCD', font=('calibri', 20, 'bold'))
-    upload = Button(top, text="Cartoonify an Image", command=upload, padx=10, pady=5)
+    upload = Button(top, text="Cartoonify an Image", command=uploadToCartoonify, padx=10, pady=5)
     upload.configure(background='#364156', foreground='white', font=('calibri', 10, 'bold'))
     upload.pack(side=TOP, pady=50)
     top.mainloop()
